90a2012290738127a82d2e0599d73692213904d7
Vladimer Redula <vdredula@gmail.com>
2021-10-05 19:08:12 +0900

    added retrieving entry function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown import Markdown
 
 from . import util
 
@@ -8,3 +9,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    content = util.get_entry(entry)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry.capitalize(),
+        "entry": Markdown.convert(content)
+    })
