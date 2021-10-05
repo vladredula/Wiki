f20f3dfc909b62f880ed70cb61d45bfb448128a8
Vladimer Redula <vdredula@gmail.com>
2021-10-05 21:06:20 +0900

    added search funtion

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,4 +7,5 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -25,3 +25,26 @@ def entry(request, entry):
         "title": title,
         "entry": md.convert(content)
     })
+
+def search(request):
+    if request.method == "POST":
+        entries = util.list_entries()
+        found = []
+
+        query = request.POST["query"]
+
+        for entry in entries:
+            if query.lower() == entry.lower():
+                content = util.get_entry(entry)
+                md = Markdown()
+
+                return render(request, "encyclopedia/entry.html", {
+                    "title": entry,
+                    "entry": md.convert(content)
+                })
+            elif query.lower() in entry.lower():
+                found.append(entry)
+        
+        return render(request, "encyclopedia/index.html", {
+            "entries": found
+        })
